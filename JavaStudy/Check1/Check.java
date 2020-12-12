8099fe364c597b45d6e3cbb2a5455dfdbfde931c
nao-cp3 <pg.cp3.lj23.clippers@gmail.com>
2020-12-12 23:57:24 +0900

    first commit

---
+++ JavaStudy/Check1/Check.java
@@ -0,0 +1,39 @@
+/**
+ * バブルソート
+ * チェックテスト-Java1章
+ *
+ */
+public class Check{
+    public static void main(String[] args){
+        /*
+        * 問1
+        * int型の配列dataを作成し、値を3,1,2,7,5で初期化しなさい
+        */
+        int[] data = { 3, 1, 2, 7, 5 };
+
+        /*
+        * 問2
+        * 以下のfor文を完成させなさい
+        */
+        for(int i = 0; i < data.length; i++){
+            System.out.print(data[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j > i; j--) {
+                /*
+                * 問3
+                * 以下、配列の添字を入れてソートを完成させなさい
+                */
+                if(data[3] > data[0]){
+                  int box = data[4];
+                  data[4] = data[4];
+                  data[3] = box;
+                }
+            }
+        }
+        for(int i = 0; i < data.length; i++){
+            System.out.print(data[i] + " ");
+        }
+    }
+}
