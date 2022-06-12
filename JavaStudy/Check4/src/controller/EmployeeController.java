db8cf179f8e760db2b17f3cf398382712f810caa
nao-cp3 <pg.cp3.lj23.clippers@gmail.com>
2022-06-13 00:22:56 +0900

    1-4

--- JavaStudy/Check4/src/controller/EmployeeController.java
+++ JavaStudy/Check4/src/controller/EmployeeController.java
@@ -24,7 +24,7 @@ public class EmployeeController extends HttpServlet {
  try {
   // 問① index.htmlから送信されたIDとPassWordの値を取得できるように修正しましょう。
  String id = request.getParameter("id");
- String password = request.getParameter("password");
+ String password = request.getParameter("pass");
  
  /*
  * IDとPassWordと元に、社員情報を検索する関数の呼び出し、結果をJSPに渡す処理
